09627671c2f48d3aeaa0537b4687bdd15baacb3f
Berylijn <berylijn@gmail.com>
2023-09-25 13:40:06 +0200

    Add navigation and links to header and add header to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/header';
 import './globals.css';
 import { Inter } from 'next/font/google';
 
@@ -16,10 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={'${inter.className} bg-gray-50 relative'}>
+      <body
+        className={
+          '${inter.className} box-border bg-gray-50 relative text-gray-950 h-[5000px] pt-28 sm:pt-36 overflow-x-hidden'
+        }
+      >
         <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[-11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-
+        <Header />
         {children}
       </body>
     </html>
