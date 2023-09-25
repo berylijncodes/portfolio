d639b164931704d6614a767ccf8c1d8d6a8c7d58
Berylijn <berylijn@gmail.com>
2023-09-25 15:43:28 +0200

    Add components to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 import Image from 'next/image';
+import Intro from '@/components/intro';
+import About from '@/components/about';
+import SectionDivider from '@/components/section-divider';
+import Projects from '@/components/projects';
 
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="flex flex-col items-center">
+      <Intro />
+      <SectionDivider />
+      <About />
+      <SectionDivider />
+      <Projects />
+    </main>
+  );
 }
