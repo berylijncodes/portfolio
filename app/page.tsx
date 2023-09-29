885da91f3353c091303c0235dba72acbbf0fc349
Berylijn <berylijn@gmail.com>
2023-09-29 10:59:57 +0200

    Add skills section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Intro from '@/components/intro';
 import About from '@/components/about';
 import SectionDivider from '@/components/section-divider';
 import Projects from '@/components/projects';
+import Skills from '@/components/skills';
 
 export default function Home() {
   return (
@@ -12,6 +13,8 @@ export default function Home() {
       <About />
       <SectionDivider />
       <Projects />
+      <SectionDivider />
+      <Skills />
     </main>
   );
 }
